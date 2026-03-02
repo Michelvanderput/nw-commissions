4fdef6d2cb1d680c5fb114a07a314a89e4bee904
Erik de Vos <mrcollaborator@gmail.com>
2026-03-02 13:58:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbzKvYpeEISEIhcWxQ0OTywfLIPSwJRYQ0UPFhjdDerN5dj5c_Z4r0ub_-HZbRX6COObIg/exec",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbziy2hLfae8i2vPsX0XiTQCYVffb1yTYB9s0kHFd3_84aUGE726dxscWdpDOGoU4zDn-w/exec",
 };
